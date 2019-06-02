332b2a3578fe931970a4124018cf0918e0adc460
GKF <gaokf0103@163.com>
2019-06-02 23:39:27 +0800

    some code

---
+++ src/common/js/jsonp.js
@@ -0,0 +1,23 @@
+import originJSONP from 'jsonp'
+
+const paramData = (data) => {
+  let url = ''
+  for (let k in data) {
+    let value = data[k] !== undefined ? data[k] : ''
+    url += `&${k}=${encodeURIComponent(value)}`
+  }
+  return url ? url.substring(1) : ''
+}
+
+export default (url, data, option) => {
+  url += (url.indexOf('?') !== -1 ? '?' : '&') + paramData(data)
+  return new Promise((resolve, reject) => {
+    originJSONP(url, option, (err, data) => {
+      if (!err) {
+        resolve(data)
+      } else {
+        reject(err)
+      }
+    })
+  })
+}
--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
+import 'babel-polyfill'
 import Vue from 'vue'
+import fastClick from 'fastclick'
 import App from './App'
 import router from './router'
 
+import 'common/stylus/index.styl'
+
+fastClick.attach(document.body)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
