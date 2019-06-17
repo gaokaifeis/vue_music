3403aa55c69a667a4ee97a08cd0483c3a188d3dc
gaokaifeis <gaokf0103@163.com>
2019-06-17 10:55:46 +0800

    修复排行榜请求参数不同的问题

--- src/api/rank.js
+++ src/api/rank.js
@@ -2,12 +2,33 @@ import axios from 'axios'
 import { commonParams } from './config'
 import { customFormatPadStart } from 'common/js/util'
 
-const formatDate = () => {
+const decludeIds = [4, 27, 201]
+
+const formatDate = (id) => {
   const date = new Date()
   const year = date.getFullYear()
-  const month = customFormatPadStart(date.getMonth() + 1, 2, '0')
-  const day = customFormatPadStart(date.getDate(), 2, '0')
-  return `${year}-${month}-${day}`
+  if (!decludeIds.includes(id)) {
+    const week = customFormatPadStart(formatWeek2(), 2, '0')
+    return `${year}_${week}`
+  } else {
+    if (date.getHours() < 10) {
+      date.setDate(date.getDate() - 1)
+    }
+    const month = customFormatPadStart(date.getMonth() + 1, 2, '0')
+    const day = customFormatPadStart(date.getDate(), 2, '0')
+    return `${year}-${month}-${day}`
+  }
+}
+
+let formatWeek2 = () => {
+  const week = 7
+  const currentdate = new Date()
+  let startDate = new Date()
+  startDate.setDate(1)
+  startDate.setMonth(0)
+  startDate.setDate(startDate.getDate() + (week - startDate.getDay() + 1) % week)
+  currentdate.setDate(currentdate.getDate() - (startDate.getDay() + week - 1) % week)
+  return (currentdate - startDate) / 3600 / 24 / 7 / 1000
 }
 
 export const getTopList = (disstid) => {
@@ -44,8 +65,8 @@ export const getMusicList = (id) => {
         'param': {
           'topId': id,
           'offset': 0,
-          'num': 20,
-          'period': formatDate()
+          'num': 100,
+          'period': formatDate(id)
         }
       },
       'comm': {'ct': 24, 'cv': 0}}
